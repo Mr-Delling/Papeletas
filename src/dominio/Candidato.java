0304ab1ba78193beffe3747a018318f46f595bc3
Mr-Delling <alejandro.santamariaizquierdo@usp.ceu.es>
2024-12-02 11:40:56 +0100

    Programa Papeleta

--- src/dominio/Candidato.java
+++ src/dominio/Candidato.java
@@ -1,5 +1,34 @@
 package dominio;
 
 public class Candidato {
-    
+    /*
+    Clase Candidato:
+    Atributos: nombre, votos
+    Métodos: incrementarVotos(), resetearVotos()
+     */
+    private String nombre;
+    private int votos;
+
+
+    //Constructores:
+    public Candidato(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+    //Getters y Setters:
+    public String getNombre() {
+        return nombre;
+    }
+    public Candidato setNombre(String nombre) {
+        this.nombre = nombre;
+        return this;
+    }
+
+
+    //Métodos de Candidato:
+    public Candidato incrementarVotos() {
+        votos++;
+        return this;
+    }
 }
